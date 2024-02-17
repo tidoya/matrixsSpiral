e3f125f7f9691a0f347893e875440ec4c040730b
tidoya <dimanm372@yandex.ru>
2024-02-17 18:27:33 +0300

    fix

--- indexnew.js
+++ indexnew.js
@@ -1,23 +1,39 @@
-function createSpiralMatrix(size) {
-  return Array.from({ length: size }, () =>
-      Array.from({ length: size }, () =>
-          (Math.floor(size + 1) / 2) % 2 === 1 ? 1 : 0
-      )
-  );
+const directionTypes ={
+  right: 'right',
+  bottom: 'bottom',
+  left: 'left',
+  top: 'top',
 }
-function spiralize (size){
-  spiralMatrix = createSpiralMatrix(size)
-  value = 1 
-  for(let i = 0; i< size/2; i++){
-    spiralMatrix[i].fill(0)
-  }
+// @args:
+// size: number
+// Создание матрицы
+function createSpiralMatrix(size){
+    const spiralMatrix = Array.from({ length: size }, () => Array(size).fill(0))
+    for(let i = 0; i<spiralMatrix.length; i++){
+          if(i===0) {
+              spiralMatrix[i].fill(1)
+              continue;
+          }
+          if(i===spiralMatrix.length-1)  {
+              spiralMatrix[i].fill(1)
+              continue;
+          }
+          spiralMatrix[i][size-1] = 1
+          if(i!==1) spiralMatrix[i][0] = 1
+    }
+    return spiralMatrix
+}
+// @args:
+// size: Размер матрицы
+function spiralize (size) {
+  if(!size) throw new Error('incorrect size')
+
+  let direction = directionTypes.right;   //Текущее движение
+  let currentPosition = [ 2, 1 ] // Текущая позиция [Текущая строка, Текущий столбец]
+  const spiralMatrix = createSpiralMatrix(size)
+
   return spiralMatrix
 }
-// Пример использования
 
-console.log(JSON.stringify(spiralize(5)).replace(/],/g, '],\n').slice(1, -1));
-console.log('""""""""');
-
-console.log(JSON.stringify(spiralize(6)).replace(/],/g, '],\n').slice(1, -1));
-// console.log(JSON.stringify(createMatrix(7)).replace(/],/g, '],\n').slice(1, -1));
-// console.log(JSON.stringify(createMatrix(8)).replace(/],/g, '],\n').slice(1, -1));
+const resultMatrix = spiralize(9);
+console.log(JSON.stringify(resultMatrix).replace(/],/g, '],\n').slice(1, -1));
